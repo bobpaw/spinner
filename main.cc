5dca5f2b3fc56bd22e776a5c4168a1bdd4d5899f
Aiden Woodruff <aidenpw@hotmail.com>
2017-10-02 17:26:06 -0500

    Geometric circle rendering
    Added code for circle equation
    Added rendering setting to render to textures
    Included <cmath>, may not need it

--- main.cc
+++ main.cc
@@ -12,15 +12,31 @@ int main (int argc, char * argv[]) {
     std::cerr << "SDL couldn't create window. SDL_error: " << SDL_GetError() << std::endl;
     return -1;
   }
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
   if (renderer == nullptr) {
     std::cerr << "SDL couldn't create renderer. SDL_error: " << SDL_GetError() << std::endl;
     return -1;
   }
-  SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+  SDL_Texture * circle = SDL_CreateTexture(renderer, SDL_GetWindowPixelFormat(window), SDL_TEXTUREACCESS_TARGET, 100, 100);
+  int circle_radius = 50;
+  int h = 50;
+  int k = 50; // (h, k) is circle center point
+  SDL_SetRenderTarget(renderer, circle);
+  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
   SDL_RenderClear(renderer);
+  SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+  for (int x = 0, y = 0; x < 100 && y < 100; x++, y++) {
+    int realx = x - 50;
+    int realy = y - 50;
+    if ((realx-h) * (realx - h) + (realy-k) * (realy-k) == (circle_radius * circle_radius) ) SDL_RenderDrawPoint(renderer, x, y);
+  }
+  SDL_SetRenderTarget(renderer, nullptr);
+  SDL_RenderClear(renderer);
+  SDL_Rect destrect = {0, 0, 100, 100};
+  SDL_RenderCopy(renderer, circle, nullptr, &destrect);
   SDL_RenderPresent(renderer);
   SDL_Delay(1000);
+  SDL_DestroyTexture(circle);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
